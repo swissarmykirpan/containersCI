e292037d7ec07ddd26e2aaeb62df3a31b3208186
Gurpreet Sohal <gurpreet@gurpreetsohal.com>
2015-06-17 14:27:51 +0100

    initial commit

---
+++ Spec/Dockerfile_spec.rb
@@ -0,0 +1,29 @@
+require "serverspec"
+require "docker"
+
+describe "Dockerfile" do
+  before(:all) do
+    image = Docker::Image.build_from_dir('.')
+
+    set :os, family: :debian
+    set :backend, :docker
+    set :docker_image, image.id
+
+  end
+
+  it "installs the correct version of Ubuntu" do
+    expect(os_version).to include("Ubuntu 14")
+  end
+
+  it "installs the correct version of Meteor" do
+    expect(meteor_version).to include("1.1.0.2")
+  end
+
+  def os_version
+    command("lsb_release -a").stdout
+  end
+
+  def meteor_version
+    command("meteor --version").stdout
+  end
+end
